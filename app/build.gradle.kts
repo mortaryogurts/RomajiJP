eeff50ee3af0674e859bb6431c859494d78cfb44
kanishk <kanishkdas19@gmail.com>
2026-06-01 17:08:26 +0530

    added Kuroshiro Library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     //coroutines2
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    //Japanese
+    implementation("com.ibm.icu:icu4j:73.2")
 }
